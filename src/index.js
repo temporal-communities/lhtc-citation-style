1cef2d75322427482935f68a08273403143550f8
v-ji <1476338+v-ji@users.noreply.github.com>
2023-02-10 16:41:30 +0100

    Dynamically generate timestamp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url'
 
 const LOCALES = ['en', 'de']
 const LOCALE_PLACEHOLDER = '{{LOCALE}}'
+const TIMESTAMP_PLACEHOLDER = '{{TIMESTAMP}}'
 
 const __filename = fileURLToPath(import.meta.url)
 const src = path.dirname(__filename)
@@ -12,6 +13,10 @@ const dist = path.join(projectRoot, 'dist')
 
 for (const locale of LOCALES) {
   const file = fs.readFileSync(path.join(src, 'lhtc.csl'), 'utf8')
-  const replaced = file.replaceAll(LOCALE_PLACEHOLDER, locale)
+
+  const replaced = file
+    .replaceAll(LOCALE_PLACEHOLDER, locale)
+    .replaceAll(TIMESTAMP_PLACEHOLDER, new Date().toISOString().substring(0, 10))
+
   fs.writeFileSync(path.join(dist, `lhtc-${locale}.csl`), replaced)
 }
