ae0b1fc46d9db261254f96f2994581f75fe7340e
v-ji <1476338+v-ji@users.noreply.github.com>
2023-02-13 15:05:04 +0100

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-const LOCALES = ['en', 'de']
-const LOCALE_PLACEHOLDER = '{{LOCALE}}'
-const TIMESTAMP_PLACEHOLDER = '{{TIMESTAMP}}'
+const LOCALES = ["en", "de"]
+const LOCALE_PLACEHOLDER = "{{LOCALE}}"
+const TIMESTAMP_PLACEHOLDER = "{{TIMESTAMP}}"
 
 const __filename = fileURLToPath(import.meta.url)
 const src = path.dirname(__filename)
-const projectRoot = path.join(src, '..')
-const dist = path.join(projectRoot, 'dist')
+const projectRoot = path.join(src, "..")
+const dist = path.join(projectRoot, "dist")
 
 for (const locale of LOCALES) {
-  const file = fs.readFileSync(path.join(src, 'lhtc.csl'), 'utf8')
+  const file = fs.readFileSync(path.join(src, "lhtc.csl"), "utf8")
 
   const replaced = file
     .replaceAll(LOCALE_PLACEHOLDER, locale)
--- test/test.js
+++ test/test.js
@@ -1,24 +1,24 @@
-import { equal } from 'assert'
-import Cite from 'citation-js'
-import { readFileSync } from 'fs'
+import { equal } from "assert"
+import Cite from "citation-js"
+import { readFileSync } from "fs"
 
 // Load LHTC style
-const templateName = 'lhtc'
-const template = readFileSync('./dist/lhtc-en.csl', 'utf8')
+const templateName = "lhtc"
+const template = readFileSync("./dist/lhtc-en.csl", "utf8")
 
-const config = Cite.plugins.config.get('@csl')
+const config = Cite.plugins.config.get("@csl")
 config.templates.add(templateName, template)
 
 // Load test data
 // Relative to script directory
-const data = readFileSync('./test/export.json', 'utf8')
+const data = readFileSync("./test/export.json", "utf8")
 
 const example = new Cite(data)
 const bib = example
-  .format('bibliography', {
-    format: 'html',
+  .format("bibliography", {
+    format: "html",
     template: templateName,
-    lang: 'en',
+    lang: "en",
     asEntryArray: true,
   })
   // Extract content of csl-entry div
@@ -32,11 +32,11 @@ const bib = example
 const bibObj = Object.fromEntries(bib)
 
 // Read reference strings
-const referenceStrings = JSON.parse(readFileSync('./test/reference-strings.json', 'utf8'))
+const referenceStrings = JSON.parse(readFileSync("./test/reference-strings.json", "utf8"))
 
 // Regex to delete surrounding <span> tags
 
-describe('Bibliography', function () {
+describe("Bibliography", function () {
   for (const [category, refObjs] of Object.entries(referenceStrings)) {
     describe(category, function () {
       for (const obj of refObjs) {
