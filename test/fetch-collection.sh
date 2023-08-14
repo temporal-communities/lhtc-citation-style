c856afe33ba17eb0c34162c9477f10de640964ec
v-ji <1476338+v-ji@users.noreply.github.com>
2023-08-14 16:09:32 +0200

    Use only top-level items

--- test/fetch-collection.sh
+++ test/fetch-collection.sh
@@ -5,10 +5,10 @@ source .env
 
 GROUP="4530458"
 COLLECTION="UCVH98NN"
-URL="https://api.zotero.org/groups/$GROUP/collections/$COLLECTION/items"
+URL="https://api.zotero.org/groups/$GROUP/collections/$COLLECTION/items/top"
 
 # Get CSL export from Zotero library
-https://www.zotero.org/support/dev/web_api/v3/basics
+# https://www.zotero.org/support/dev/web_api/v3/basics
 curl --get $URL \
   -H "Authorization: Bearer $ZOTERO_API_KEY" \
   --data-urlencode "format=csljson"
