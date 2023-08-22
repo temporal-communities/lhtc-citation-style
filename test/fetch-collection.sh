21210d2247c9ded2ce9562256c5d700ba7b10e86
v-ji <1476338+v-ji@users.noreply.github.com>
2023-08-22 12:59:24 +0200

    Add limit parameter

--- test/fetch-collection.sh
+++ test/fetch-collection.sh
@@ -6,10 +6,12 @@ source .env
 GROUP="4530458"
 COLLECTION="UCVH98NN"
 URL="https://api.zotero.org/groups/$GROUP/collections/$COLLECTION/items/top"
+LIMIT=100 # Maxiumum number of items supported
 
 # Get CSL export from Zotero library
 # https://www.zotero.org/support/dev/web_api/v3/basics
 curl --get $URL \
   -H "Authorization: Bearer $ZOTERO_API_KEY" \
-  --data-urlencode "format=csljson"
+  --data-urlencode "format=csljson" \
+  --data-urlencode "limit=$LIMIT" 
 
